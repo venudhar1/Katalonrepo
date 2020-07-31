a66028cf94d2c1a067a8605f23108e504d8f42a9
venudhar1 <logic.venu3@live.in>
2020-07-31 19:03:30 +0530

    ok

--- Scripts/VerifyLogin/Script1596199305786.groovy
+++ Scripts/VerifyLogin/Script1596199305786.groovy
@@ -24,8 +24,7 @@ WebUI.maximizeWindow()
 
 WebUI.setText(findTestObject('Object Repository/Page_Sign In/input_Sign in with your organizational acco_d4c899'), 'testautomation@VTV9R')
 
-WebUI.setEncryptedText(findTestObject('Object Repository/Page_Sign In/input_Sign in with your organizational acco_04b8f7'), 
-    'grud1T4tYSPKRURLQUljPA==')
+WebUI.sendKeys(findTestObject('Object Repository/Page_Sign In/input_Sign in with your organizational acco_04b8f7'), '1qaz!QAZ')
 
 WebUI.click(findTestObject('Object Repository/Page_Sign In/span_Sign in'))
 
